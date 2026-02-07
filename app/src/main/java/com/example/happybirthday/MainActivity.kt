7c38758d92f1e0d7e47dc3935d3dcfec2cde303e
ViyanKo <alinakus2007@gmail.com>
2026-02-07 10:00:56 +0300

    Add greeting text

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Alina",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                //}
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text=message,
+        fontSize = 100.sp, //наложение текста из-за отсутствия высоты строки
+        lineHeight = 116.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText(message = "Happy birthday Alina!")
+    }
+}
