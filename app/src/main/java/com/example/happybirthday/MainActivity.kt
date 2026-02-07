b2d58509e6e92b4935f56cfa78c7de828d8966d4
ViyanKo <alinakus2007@gmail.com>
2026-02-07 10:38:05 +0300

    Update greeting text

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -20,30 +28,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Alina",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-                //}
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+                    GreetingText(
+                        message = "Happy birthday Alina!",
+                        from = "from: Alina",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text=message,
-        fontSize = 100.sp, //наложение текста из-за отсутствия высоты строки
-        lineHeight = 116.sp
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier//.fillMaxSize() //подстраивается под максимальный размер экрана
     )
+
+    {
+        Text(
+            text = message, //сообщение
+            fontSize = 100.sp, //наложение текста из-за отсутствия высоты строки
+            lineHeight = 116.sp, //высота строки
+            textAlign = TextAlign.Center
+        )
+        Text (
+            text = from, //от кого
+            fontSize = 36.sp, //размер шрифта
+            modifier = Modifier
+                .padding(16.dp) //отступ от края
+                .align(alignment = Alignment.End) //выравниваем в конце
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy birthday Alina!")
+        GreetingText(message = "Happy birthday Alina!", from = "from: Alina")
     }
 }
